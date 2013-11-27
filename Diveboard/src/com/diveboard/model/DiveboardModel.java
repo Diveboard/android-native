6fae822fc43bfaa58a4f6e06ff19262678b85c5c
Jean <taihaut@diveboard.com>
2013-11-27 13:32:41 +0100

    Fixing null pointer on null profile image

--- Diveboard/src/com/diveboard/model/DiveboardModel.java
+++ Diveboard/src/com/diveboard/model/DiveboardModel.java
@@ -563,7 +563,8 @@ public class					DiveboardModel
 		ArrayList<Dive> dives = _user.getDives();
 		for (int i = 0, len = dives.size(); i < len; i++)
 		{
-			dives.get(i).getProfile().deletePicture(_context);
+			if (dives.get(i).getProfile() != null)
+				dives.get(i).getProfile().deletePicture(_context);
 		}
 		_enable_overwrite = false;
 		_applyEdit();
