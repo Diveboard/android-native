3893b96397f8d3198cc53f2dc3c9a90a982c1004
taihaut <taihaut@diveboard.com>
2013-12-16 15:53:55 +0100

    Adding Ant build

--- Diveboard/src/Config.java
+++
@@ -1,7 +0,0 @@
-package com.diveboard.config;
-
-public class					AppConfig
-{
-	public static final			String APP_ID = "@CONFIG.APP_ID@";
-	public static final			Boolean RELEASE = @CONFIG.RELEASE@;
-}
