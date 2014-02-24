65673178c52e9b6ea392caa80853b9e0698263ae
suiko <suiko@diveboard.com>
2014-02-24 12:00:15 +0100

    TabEditBuddies updated

--- Diveboard/src/com/diveboard/mobile/editdive/TabEditBuddiesFragment.java
+++ Diveboard/src/com/diveboard/mobile/editdive/TabEditBuddiesFragment.java
@@ -5,15 +5,19 @@ import com.diveboard.mobile.R;
 
 import java.util.ArrayList;
 
+import org.apache.commons.codec.digest.Md5Crypt;
+
 import com.diveboard.model.Buddy;
 import com.diveboard.model.Dive;
 import com.diveboard.model.DiveboardModel;
+import com.diveboard.util.ExpandableHeightGridView;
 import com.diveboard.util.ImageCache.ImageCacheParams;
 import com.diveboard.util.ImageFetcher;
 import com.diveboard.util.Utils;
 
 import android.support.v4.app.Fragment;
 import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,13 +26,22 @@ import android.view.ViewTreeObserver;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.AbsListView.OnScrollListener;
 import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class					TabEditBuddiesFragment extends Fragment
 {
-	private GridView			mGridView;
+	private ExpandableHeightGridView			mGridView;
+	private ExpandableHeightGridView			mGridViewBuddies;
 	private int					mIndex;
 	private DiveboardModel		mModel;
 	private static final String TAG = "ImageGridFragment";
@@ -37,8 +50,11 @@ public class					TabEditBuddiesFragment extends Fragment
     private int mImageThumbSpacing;
     private ImageFetcher mImageFetcher;
     private ImageAdapter mAdapter;
+    private ImageAdapterBuddies mAdapterBuddies;
     ArrayList<Buddy> mOldBuddies;
+    ArrayList<Buddy> mBuddies;
     ArrayList<String> mListString = new  ArrayList<String>();
+    private ViewGroup mRootView;
     
     public TabEditBuddiesFragment() {
     	
@@ -55,6 +71,7 @@ public class					TabEditBuddiesFragment extends Fragment
 		mIndex = getActivity().getIntent().getIntExtra("index", -1);
 		setHasOptionsMenu(false);
 		mAdapter = new ImageAdapter(getActivity());
+		mAdapterBuddies = new ImageAdapterBuddies(getActivity());
 		mImageThumbSize = getResources().getDimensionPixelSize(R.dimen.image_thumbnail_size);
 		mImageThumbSpacing = getResources().getDimensionPixelSize(R.dimen.image_thumbnail_spacing);
 
@@ -70,21 +87,76 @@ public class					TabEditBuddiesFragment extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance)
     {
     	ApplicationController AC = (ApplicationController)getActivity().getApplicationContext();
-    	mOldBuddies = AC.getModel().getOldBuddies();
-    	for (int i = 0; i < mOldBuddies.size(); i++)
-    	{
-    		System.out.println("Add String " + mOldBuddies.get(i).getPicture()._urlDefault);
-    		mListString.add(mOldBuddies.get(i).getPicture()._urlDefault);
-    	}
-    	for (int i = 0; i < mOldBuddies.size(); i++)
-        {
-        	System.out.println("TEST: "+ mOldBuddies.get(i).getNickname() + " " + mOldBuddies.get(i).getId());
-        }
-    	final ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.tab_edit_buddies, container, false);
-    	mGridView = (GridView)rootView.findViewById(R.id.gridView);
     	Dive dive = mModel.getDives().get(mIndex);
+    	mOldBuddies = AC.getModel().getOldBuddies();
+    	mBuddies = dive.getBuddies();
+//    	for (int i = 0; i < mOldBuddies.size(); i++)
+//    	{
+//    		System.out.println("Add String " + mOldBuddies.get(i).getPicture()._urlDefault);
+//    		mListString.add(mOldBuddies.get(i).getPicture()._urlDefault);
+//    	}
+//    	for (int i = 0; i < mOldBuddies.size(); i++)
+//        {
+//        	System.out.println("TEST: "+ mOldBuddies.get(i).getNickname() + " " + mOldBuddies.get(i).getId());
+//        }
+    	mRootView = (ViewGroup) inflater.inflate(R.layout.tab_edit_buddies, container, false);
+    	mGridView = (ExpandableHeightGridView)mRootView.findViewById(R.id.gridView);
+    	mGridViewBuddies = (ExpandableHeightGridView)mRootView.findViewById(R.id.gridViewBuddies);
     	mGridView.setAdapter(mAdapter);
+    	mGridView.setExpanded(true);
+    	mGridViewBuddies.setExpanded(true);
+    	mGridViewBuddies.setAdapter(mAdapterBuddies);
     	mGridView.setOnScrollListener(new OnScrollListener() {
+			
+			@Override
+			public void onScrollStateChanged(AbsListView view, int scrollState) {
+				// Pause fetcher to ensure smoother scrolling when flinging
+                if (scrollState == AbsListView.OnScrollListener.SCROLL_STATE_FLING) {
+                    // Before Honeycomb pause image loading on scroll to help with performance
+                    if (!Utils.hasHoneycomb()) {
+                        mImageFetcher.setPauseWork(true);
+                    }
+                } else {
+                    mImageFetcher.setPauseWork(false);
+                    
+                }
+			}
+			
+			@Override
+			public void onScroll(AbsListView view, int firstVisibleItem,
+					int visibleItemCount, int totalItemCount) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+    	mGridViewBuddies.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int position,
+					long arg3) {
+				if (!mOldBuddies.contains(mBuddies.get(position)))
+					mOldBuddies.add(mBuddies.get(position));
+				mBuddies.remove(position);
+				mAdapter.notifyDataSetChanged();
+				mAdapterBuddies.notifyDataSetChanged();
+				mModel.getDives().get(mIndex).setBuddies(mBuddies);
+			}
+		});
+    	mGridView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View v, int position,
+					long id) {
+				System.out.println(position);
+				if (!mBuddies.contains(mOldBuddies.get(position)))
+					mBuddies.add(mOldBuddies.get(position));
+				mOldBuddies.remove(position);
+				mAdapter.notifyDataSetChanged();
+				mAdapterBuddies.notifyDataSetChanged();
+				mModel.getDives().get(mIndex).setBuddies(mBuddies);
+			}
+		});
+    	mGridViewBuddies.setOnScrollListener(new OnScrollListener() {
 			
 			@Override
 			public void onScrollStateChanged(AbsListView view, int scrollState) {
@@ -140,35 +212,79 @@ public class					TabEditBuddiesFragment extends Fragment
                         }
                     }
                 });
-		return rootView;
+        mGridViewBuddies.getViewTreeObserver().addOnGlobalLayoutListener(
+                new ViewTreeObserver.OnGlobalLayoutListener() {
+                    @Override
+                    public void onGlobalLayout()
+                    {
+                    	
+                        if (mAdapter.getNumColumns() == 0)
+                        {
+                            final int numColumns = (int) Math.floor(
+                            		mGridViewBuddies.getWidth() / (mImageThumbSize + mImageThumbSpacing));
+                            if (numColumns > 0) {
+                                final int columnWidth =
+                                        (mGridViewBuddies.getWidth() / numColumns) - mImageThumbSpacing;
+                                //mAdapter.setNumColumns(numColumns);
+                                mAdapterBuddies.setItemHeight(columnWidth);
+//                                if (BuildConfig.DEBUG) {
+//                                    Log.d(TAG, "onCreateView - numColumns set to " + numColumns);
+//                                }
+                                if (Utils.hasJellyBean()) {
+                                	mGridViewBuddies.getViewTreeObserver()
+                                            .removeOnGlobalLayoutListener(this);
+                                } else {
+                                	mGridViewBuddies.getViewTreeObserver()
+                                            .removeGlobalOnLayoutListener(this);
+                                }
+                            }
+                        }
+                    }
+                });
+        Typeface faceB = Typeface.createFromAsset(getActivity().getAssets(), "fonts/Quicksand-Bold.otf");
+		Typeface faceR = Typeface.createFromAsset(getActivity().getAssets(), "fonts/Quicksand-Regular.otf");
+        ((TextView)mRootView.findViewById(R.id.myBuddies)).setTypeface(faceB);
+        ((TextView)mRootView.findViewById(R.id.myOldBuddies)).setTypeface(faceB);
+        ((TextView)mRootView.findViewById(R.id.search)).setTypeface(faceB);
+        ((TextView)mRootView.findViewById(R.id.diveboard_text)).setTypeface(faceB);
+        ((EditText)mRootView.findViewById(R.id.diveboard_edit)).setTypeface(faceR);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+				R.array.buddy_short_array, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner spinner = ((Spinner)mRootView.findViewById(R.id.spinner));
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View arg1,
+					int position, long arg3) {
+				if (position == 0)
+				{
+//					EditText editText = new EditText(getActivity());
+//					RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+//					params.addRule(RelativeLayout.BELOW, arg0.getId());
+//					editText.setLayoutParams(params);
+//					((RelativeLayout)mRootView.findViewById(R.id.parent_rl)).addView(editText);
+				}
+				else if (position == 1)
+				{
+					
+				
+				}
+				else if (position == 2)
+				{
+					
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+        return mRootView;
     }
-    
-//    @Override
-//    public void onResume() {
-//    	super.onResume();
-//        mImageFetcher.setExitTasksEarly(false);
-//        mAdapter.notifyDataSetChanged();
-//    }
-//
-//    @Override
-//    public void onPause() {
-//        super.onPause();
-//        System.out.println("ON-PAUSE");
-//        mImageFetcher.setPauseWork(false);
-//        mImageFetcher.setExitTasksEarly(true);
-//        mImageFetcher.flushCache();
-//    }
-//
-//    @Override
-//    public void onDestroy() {
-//        super.onDestroy();
-//        System.out.println("ON-DESTROY");
-//        //mImageFetcher.closeCache();
-//        mImageFetcher.setPauseWork(false);
-//        mImageFetcher.setExitTasksEarly(true);
-//        mImageFetcher.flushCache();
-//        mImageFetcher.closeCache();
-//    }
     
     @Override
     public void onResume() {
@@ -209,60 +325,17 @@ public class					TabEditBuddiesFragment extends Fragment
             mContext = context;
             mImageViewLayoutParams = new GridView.LayoutParams(
                     LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-            
-            ApplicationController AC = (ApplicationController)mContext.getApplicationContext();
-            // Calculate ActionBar height
-//            TypedValue tv = new TypedValue();
-//            if (context.getTheme().resolveAttribute(
-//                    android.R.attr.actionBarSize, tv, true)) {
-//                mActionBarHeight = TypedValue.complexToDimensionPixelSize(
-//                        tv.data, context.getResources().getDisplayMetrics());
-//            }
         }
 
         @Override
         public int getCount() {
-            // If columns have yet to be determined, return no items
-//            if (getNumColumns() == 0) {
-//                return 0;
-//            }
-
-            // Size + number of columns for top empty row
-            //return Images.imageThumbUrls.length + mNumColumns;
-//        	ApplicationController AC = (ApplicationController)mContext.getApplicationContext();
-        	//return mListString.size();
         	return mOldBuddies.size();
-        	//return mOldBuddies.size();
         }
-
-//        @Override
-//        public Object getItem(int position) {
-//        	//return Images.imageThumbUrls[position - mNumColumns];
-//        	ApplicationController AC = (ApplicationController)mContext.getApplicationContext();
-//        	try {
-//				return (Bitmap)AC.getModel().getOldBuddies().get(position).getPicture().getPicture(mContext);
-//			} catch (IOException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//        	return null;
-//        }
 
         @Override
         public long getItemId(int position) {
             return position - mNumColumns;
         }
-
-//        @Override
-//        public int getViewTypeCount() {
-//            // Two types of views, the normal ImageView and the top row of empty views
-//            return 1;
-//        }
-//
-//        @Override
-//        public int getItemViewType(int position) {
-//            return 0;
-//        }
 
         @Override
         public boolean hasStableIds() {
@@ -271,16 +344,6 @@ public class					TabEditBuddiesFragment extends Fragment
 
         @Override
         public View getView(int position, View convertView, ViewGroup container) {
-            // First check if this is the top row
-//            if (position < mNumColumns) {
-//                if (convertView == null) {
-//                    convertView = new View(mContext);
-//                }
-//                // Set empty view with height of ActionBar
-//                convertView.setLayoutParams(new AbsListView.LayoutParams(
-//                        ViewGroup.LayoutParams.MATCH_PARENT, mActionBarHeight));
-//                return convertView;
-//            }
 
             // Now handle the main ImageView thumbnails
             ImageView imageView;
@@ -302,11 +365,7 @@ public class					TabEditBuddiesFragment extends Fragment
             // Finally load the image asynchronously into the ImageView, this also takes care of
             // setting a placeholder image while the background thread runs
             ApplicationController AC = (ApplicationController)mContext.getApplicationContext();
-            
-            
-            //mImageFetcher.loadImage(mListString.get(position), imageView);
-            //mImageFetcher.loadImage(Images.imageThumbUrls[position], imageView);
-            if (Images.imageTest[position].contains("no_picture"))
+            if (mOldBuddies.get(position).getPicture()._urlDefault.contains("no_picture"))
             {
             	imageView.setImageResource(R.drawable.no_picture);
             }
@@ -343,6 +402,99 @@ public class					TabEditBuddiesFragment extends Fragment
 		@Override
 		public Object getItem(int position) {
 			return mOldBuddies.get(position).getPicture()._urlDefault;
+		}
+    }
+    
+    
+    private class ImageAdapterBuddies extends BaseAdapter {
+
+        private final Context mContext;
+        private int mItemHeight = 0;
+        private int mNumColumns = 0;
+        private GridView.LayoutParams mImageViewLayoutParams;
+
+        public ImageAdapterBuddies(Context context) {
+            super();
+            mContext = context;
+            mImageViewLayoutParams = new GridView.LayoutParams(
+                    LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        }
+
+        @Override
+        public int getCount() {
+        	return mBuddies.size();
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position - mNumColumns;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup container) {
+
+            // Now handle the main ImageView thumbnails
+            ImageView imageView;
+            if (convertView == null) { // if it's not recycled, instantiate and initialize
+            	System.out.println("convertview = null");
+                imageView = new RecyclingImageView(mContext);
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setLayoutParams(mImageViewLayoutParams);
+            } else { // Otherwise re-use the converted view
+            	System.out.println("convertview != null");
+            	imageView = (ImageView) convertView;
+            }
+
+            // Check the height matches our calculated column width
+            if (imageView.getLayoutParams().height != mItemHeight) {
+                imageView.setLayoutParams(mImageViewLayoutParams);
+            }
+
+            // Finally load the image asynchronously into the ImageView, this also takes care of
+            // setting a placeholder image while the background thread runs
+            ApplicationController AC = (ApplicationController)mContext.getApplicationContext();
+            if (mBuddies.get(position).getPicture()._urlDefault.contains("no_picture"))
+            {
+            	imageView.setImageResource(R.drawable.no_picture);
+            }
+            else
+            	mImageFetcher.loadImage(mBuddies.get(position).getPicture()._urlDefault, imageView);
+            return imageView;
+        }
+
+        /**
+         * Sets the item height. Useful for when we know the column width so the height can be set
+         * to match.
+         *
+         * @param height
+         */
+        public void setItemHeight(int height) {
+            if (height == mItemHeight) {
+                return;
+            }
+            mItemHeight = height;
+            mImageViewLayoutParams =
+                    new GridView.LayoutParams(LayoutParams.MATCH_PARENT, mItemHeight);
+            mImageFetcher.setImageSize(height);
+            notifyDataSetChanged();
+        }
+
+        public void setNumColumns(int numColumns) {
+            mNumColumns = numColumns;
+        }
+
+        public int getNumColumns() {
+            return mNumColumns;
+        }
+
+		@Override
+		public Object getItem(int position) {
+			return mBuddies.get(position).getPicture()._urlDefault;
 		}
     }
 }
