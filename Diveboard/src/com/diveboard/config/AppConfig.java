e1c869d6d666eaf9fbe02a2c82b172da44e8f53c
Gonzalo Gran <gongracr@gmail.com>
2014-04-23 10:24:50 +0200

    Merged with previous version of the manifest

--- Diveboard/src/com/diveboard/config/AppConfig.java
+++ Diveboard/src/com/diveboard/config/AppConfig.java
@@ -2,7 +2,7 @@ package com.diveboard.config;
 
 public class					AppConfig
 {
-	public static final String	SERVER_URL = "http://www.diveboard.com";
+	public static final String	SERVER_URL = "http://stage.diveboard.com";
 	public static final String	BUGSENSE_ID = "0c480522";
 	public static final Integer	DEBUG_MODE = 0;
 }
