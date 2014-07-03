711dab1839323d35e3bd849be7fb747cf05b355d
Gonzalo Gran <gonzalo@diveboard.com>
2014-07-03 16:47:36 +0200

    DrawerNavigation rolled back and commented

--- Diveboard/src/com/diveboard/mobile/AppRater.java
+++ Diveboard/src/com/diveboard/mobile/AppRater.java
@@ -27,10 +27,10 @@ public class AppRater {
         	AC.getModel().setHasRatedApp(false);
         }
 
-//        if (AC.getModel().hasRatedApp()) { 
-//        	System.out.println("User has already rated the app");
-//        	return ; 
-//        }
+        if (AC.getModel().hasRatedApp()) { 
+        	System.out.println("User has already rated the app");
+        	return ; 
+        }
         
         // Increment launch counter
         Long launch_count = AC.getModel().getLaunchCount() + 1L;
